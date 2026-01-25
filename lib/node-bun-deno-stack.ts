3a48a467f2ecc1455e40cc1a9a50246301362991
C F <carl@otsu.se>
2026-01-25 19:14:47 +0100

    add bun skeleton

--- lib/node-bun-deno-stack.ts
+++ lib/node-bun-deno-stack.ts
@@ -1,16 +1,23 @@
-import * as cdk from 'aws-cdk-lib/core';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib/core";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { Platform } from "aws-cdk-lib/aws-ecr-assets";
 
 export class NodeBunDenoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'NodeBunDenoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new lambda.DockerImageFunction(this, "BunLambda", {
+      code: lambda.DockerImageCode.fromImageAsset("src/bun", {
+        platform: Platform.LINUX_AMD64,
+      }),
+      architecture: lambda.Architecture.X86_64,
+      memorySize: 128,
+      timeout: cdk.Duration.seconds(30),
+      environment: {
+        AWS_LWA_READINESS_CHECK_PATH: "/",
+        AWS_LWA_INVOKE_MODE: "buffered",
+      },
+    });
   }
 }
