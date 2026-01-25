2ed7a7f05fbc4da1d5da809beac6bae021dad0ed
C F <carl@otsu.se>
2026-01-25 16:19:20 +0100

    Initial commit

---
+++ test/node-bun-deno.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as NodeBunDeno from '../lib/node-bun-deno-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/node-bun-deno-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new NodeBunDeno.NodeBunDenoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
